43e6b5650d700e1707009c5dfc86a1a974ed7d57
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2021-01-03 11:26:34 +0100

    added 'Infrastructure' project with an applicationDbContext

---
+++ Hahn.ApplicatonProcess.Application/Infrastructure/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Hahn.ApplicatonProcess.December2020.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public DbSet<Applicant> Applicant { get; set; }
+    }
+}

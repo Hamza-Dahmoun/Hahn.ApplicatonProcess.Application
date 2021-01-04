64d5149140307bb2e8af968c64aa75a7a21f0e99
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2021-01-04 10:32:51 +0100

    add ApplicantController & register applicantBusinessService inside the api project

---
+++ Hahn.ApplicatonProcess.Application/API/Controllers/ApplicantController.cs
@@ -0,0 +1,21 @@
+﻿using Hahn.ApplicatonProcess.December2020.Domain.Business.BusinessServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicantController : ControllerBase
+    {
+        protected readonly ApplicantBusinessService _applicantBusinessService;
+        public ApplicantController(ApplicantBusinessService applicantBusinessService)
+        {
+            _applicantBusinessService = applicantBusinessService;
+        }
+    }
+}
--- Hahn.ApplicatonProcess.Application/API/Startup.cs
+++ Hahn.ApplicatonProcess.Application/API/Startup.cs
@@ -1,3 +1,4 @@
+using Hahn.ApplicatonProcess.December2020.Domain.Business.BusinessServices;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +29,10 @@ namespace API
         {
 
             services.AddControllers();
+
+            //registering ApplicantBusinessService
+            services.AddTransient<ApplicantBusinessService>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });

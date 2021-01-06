c41a1beaccfffc67852f5223dbc63937c95aa870
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2021-01-06 16:28:09 +0100

    added ApplicantController with Index action to MVC project

---
+++ Hahn.ApplicatonProcess.Application/MVC/Controllers/ApplicantController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.ApiServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Controllers
+{
+    public class ApplicantController : Controller
+    {
+        private readonly ApplicantApiService _applicantApiService;
+
+        public ApplicantController(ApplicantApiService applicantApiService)
+        {
+            _applicantApiService = applicantApiService;
+        }
+
+        public IActionResult Index()
+        {
+            var applicants = _applicantApiService.GetAllAsync();
+            return View(applicants);
+        }
+    }
+}

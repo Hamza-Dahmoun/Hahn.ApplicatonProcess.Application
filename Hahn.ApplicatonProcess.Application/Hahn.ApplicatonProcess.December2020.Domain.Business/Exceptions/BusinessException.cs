7d3d428420db2401d98ae3df2d37adcbd533675a
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2021-01-06 14:26:43 +0100

    add BusinessException.cs to Domain.Business project

---
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.December2020.Domain.Business/Exceptions/BusinessException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.December2020.Domain.Business.Exceptions
+{
+    public class BusinessException:Exception
+    {
+        //We'll be using this exception whenever a business rule is not satisfied before doing changes to the db
+        public BusinessException(string message):base(message)
+        {
+
+        }
+    }
+}

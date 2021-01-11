567d601fb731d1dac77d4c63404bde63db0d56b0
hamza-dahmoun <hamza.dahmoun7@gmail.com>
2021-01-11 01:58:18 +0100

    removed localization of logs

--- Hahn.ApplicatonProcess.Application/API/Controllers/ApplicantController.cs
+++ Hahn.ApplicatonProcess.Application/API/Controllers/ApplicantController.cs
@@ -35,14 +35,14 @@ namespace API.Controllers
         [HttpGet]
         public List<ApplicantDTO> Get()
         {
-            _logger.LogInformation(_localizer["called applicant/get"]);
+            _logger.LogInformation("called applicant/get");
             return _applicantBusinessService.GetAll().Select(applicant =>applicant.AsDTO()).ToList();
         }
 
         [HttpGet("{id}")]
         public ActionResult<ApplicantDTO> Get(int id)
         {
-            _logger.LogInformation(_localizer["called applicant/get/id"]);
+            _logger.LogInformation("called applicant/get/id");
             var applicant = _applicantBusinessService.GetById(id);
             if (applicant == null)
             {
@@ -60,20 +60,20 @@ namespace API.Controllers
             
             try
             {
-                _logger.LogInformation(_localizer["called applicant/delete/id"]);
+                _logger.LogInformation("called applicant/delete/id");
                 var applicant = _applicantBusinessService.GetById(id);
                 if (applicant == null)
                 {
-                    _logger.LogWarning(_localizer["applicant to delete with id "] + id.ToString() + _localizer[" is NotFound!"]);
+                    _logger.LogWarning("applicant to delete with id " + id.ToString() + " is NotFound!");
                     return NotFound();
                 }
                 _applicantBusinessService.Delete(applicant);
-                _logger.LogInformation(_localizer["applicant with id "] + id.ToString() + _localizer[" is deleted successfully"]);
+                _logger.LogInformation("applicant with id " + id.ToString() + " is deleted successfully");
                 return NoContent();
             }
             catch (DataNotUpdatedException E)
             {
-                _logger.LogError(_localizer["Server error! could not delete applicant with id "] + id.ToString());
+                _logger.LogError("Server error! could not delete applicant with id " + id.ToString());
                 return StatusCode(500, _localizer["Server error! "] + E.Message);
             }
             catch (Exception E)
@@ -88,11 +88,11 @@ namespace API.Controllers
         {
             try
             {
-                _logger.LogInformation(_localizer["called applicant/update/id"]);
+                _logger.LogInformation("called applicant/update/id");
                 var existingApplicant = _applicantBusinessService.GetById(id);
                 if (existingApplicant == null)
                 {
-                    _logger.LogWarning(_localizer["applicant to update with id "] + id.ToString() + _localizer[" is NotFound!"]);
+                    _logger.LogWarning("applicant to update with id " + id.ToString() + " is NotFound!");
                     return NotFound();
                 }
 
@@ -105,22 +105,22 @@ namespace API.Controllers
                 existingApplicant.Hired = applicantDTO.Hired;
 
                 _applicantBusinessService.Update(existingApplicant);
-                _logger.LogInformation(_localizer["applicant with id "] + id.ToString() + _localizer[" is updated successfully"]);
+                _logger.LogInformation("applicant with id " + id.ToString() + " is updated successfully");
                 return NoContent();
             }
             catch (BusinessException E)
             {
-                _logger.LogError(_localizer["business rule not met when trying to update applicant with id "] + id.ToString() + ". " + E.Message);
+                _logger.LogError("business rule not met when trying to update applicant with id " + id.ToString() + ". " + E.Message);
                 return StatusCode(400, _localizer["Server error! "] + E.Message);
             }
             catch (DataNotUpdatedException E)
             {
-                _logger.LogError(_localizer["Server error! could not update applicant with id "] + id.ToString());
+                _logger.LogError("Server error! could not update applicant with id " + id.ToString() + ". " + E.Message);
                 return StatusCode(500, _localizer["Server error! "] + E.Message);
             }
             catch (Exception E)
             {
-                _logger.LogError(_localizer["Server error! "] + E.Message);
+                _logger.LogError("Server error! " + E.Message);
                 return StatusCode(500, _localizer["Server error!"]);
             }
         }
@@ -130,7 +130,7 @@ namespace API.Controllers
         {
             try
             {
-                _logger.LogInformation(_localizer["called applicant/create"]);
+                _logger.LogInformation("called applicant/create");
                 Applicant applicant = new()
                 {
                     Name = applicantDTO.Name,
@@ -143,22 +143,22 @@ namespace API.Controllers
                 };
 
                 _applicantBusinessService.Add(applicant);
-                _logger.LogInformation(_localizer["applicant with id "] + applicant.ID.ToString() + _localizer[" is inserted successfully"]);
+                _logger.LogInformation("applicant with id " + applicant.ID.ToString() + " is inserted successfully");
                 return CreatedAtAction(nameof(Get), new { applicant.ID }, applicant);
             }
             catch (BusinessException E)
             {
-                _logger.LogError(_localizer["business rule not met when trying to insert new applicant. "] + E.Message);
+                _logger.LogError("business rule not met when trying to insert new applicant. " + E.Message);
                 return StatusCode(400, _localizer["Server error! "] + E.Message);
             }
             catch (DataNotUpdatedException E)
             {
-                _logger.LogError(_localizer["server error! could not insert the new applicant"]);
+                _logger.LogError("server error! could not insert the new applicant");
                 return StatusCode(500, _localizer["Server error! "] + E.Message);
             }
             catch (Exception E)
             {
-                _logger.LogError(_localizer["server error! "] + E.Message);
+                _logger.LogError("server error! " + E.Message);
                 return StatusCode(500, _localizer["Server error! Create operation failed."]);
             }
         }
